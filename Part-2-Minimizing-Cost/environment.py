0be6a8691b7297e66ee15b9dcb301e7988085333
christian-ruiz <62626375+christian-ruiz@users.noreply.github.com>
2023-05-18 01:14:15 -0500

    updating the class data

--- Part-2-Minimizing-Cost/environment.py
+++ Part-2-Minimizing-Cost/environment.py
@@ -12,7 +12,7 @@ class Environment(object):
     def __init__(self, optimal_temperature = (18.0, 24.0), initial_month=0, initial_number_users = 10, initial_rate_data = 60):
         self.monthly_atmospheric_temperatures = [1.0, 5.0, 7.0, 10.0, 11.0, 20.0, 23.0, 24.0, 22.0, 10.0, 5.0, 1.0]
         self.initial_month = initial_month
-        self.atmospheric_temperatures - self.monthly_atmospheric_temperatures[initial_month]
+        self.atmospheric_temperature - self.monthly_atmospheric_temperatures[initial_month]
         self.optimal_temperature = optimal_temperature
         self.min_temperature = -20
         self.max_temperature = 80
@@ -26,9 +26,11 @@ class Environment(object):
         self.current_number_users = initial_number_users
         self.initial_rate_data = initial_rate_data
         self.current_rate_data = initial_rate_data
-
+        self.intrinsic_temperature = self.atmospheric_temperature + 1.25 +
     # MAKING A METHOD THAT UPDATES THE ENVIRONMENT RIGHT AFTER THE AI PLAYS AN ACTION
 
+
     # MAKING A METHOD THAT RESETS THE ENVIRONMENT
+
 
     # MAKING A METHOD THAT GIVES US AT ANY TIME THE CURRENT STATE, THE LAST REWARD AND WHETHER THE GAME IS OVER
